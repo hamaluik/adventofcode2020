a950616bbefa620678d5fc782ffb69124145a243
Kenton Hamaluik <kenton@hamaluik.ca>
2020-12-11 07:42:36 -0700

    day11

---
+++ day11/b/src/main.rs
@@ -0,0 +1,122 @@
+#[derive(Copy, Clone, Eq, PartialEq)]
+enum State {
+    Floor,
+    Empty,
+    Occupied,
+}
+
+impl From<char> for State {
+    fn from(c: char) -> State {
+        match c {
+            '.' => State::Floor,
+            'L' => State::Empty,
+            '#' => State::Occupied,
+            _ => panic!("bad map: {}", c),
+        }
+    }
+}
+
+#[derive(Clone, Eq, PartialEq)]
+struct Map(Vec<Vec<State>>);
+
+const DIRECTIONS: [(isize, isize); 8] = [
+    (-1, -1),
+    (0, -1),
+    (1, -1),
+    (-1, 0),
+    (1, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1),
+];
+
+impl Map {
+    fn size(&self) -> (usize, usize) {
+        (self.0[0].len(), self.0.len())
+    }
+
+    fn at(&self, loc: (isize, isize), dir: (isize, isize)) -> Option<State> {
+        let x: isize = loc.0 + dir.0;
+        let y: isize = loc.1 + dir.1;
+        let (w, h) = self.size();
+
+        if x < 0 || x as usize >= w || y < 0 || y as usize >= h {
+            None
+        } else {
+            Some(self.0[y as usize][x as usize])
+        }
+    }
+
+    fn at_los(&self, loc: (usize, usize), dir: (isize, isize)) -> State {
+        let (mut x, mut y) = (loc.0 as isize, loc.1 as isize);
+        while let Some(s) = self.at((x, y), dir) {
+            match s {
+                State::Empty | State::Occupied => return s,
+                State::Floor => {
+                    x += dir.0;
+                    y += dir.1;
+                }
+            }
+        }
+        State::Floor
+    }
+
+    fn count_neighours(&self, loc: (usize, usize)) -> usize {
+        DIRECTIONS
+            .iter()
+            .filter(|&d| self.at_los(loc, *d) == State::Occupied)
+            .count()
+    }
+
+    fn tick(&self) -> Map {
+        let mut new = self.clone();
+        let (w, h) = self.size();
+        for y in 0..h {
+            for x in 0..w {
+                new.0[y][x] = match self.0[y][x] {
+                    State::Floor => State::Floor,
+                    State::Empty => match self.count_neighours((x, y)) {
+                        0 => State::Occupied,
+                        _ => State::Empty,
+                    },
+                    State::Occupied => match self.count_neighours((x, y)) {
+                        0 | 1 | 2 | 3 | 4 => State::Occupied,
+                        _ => State::Empty,
+                    },
+                }
+            }
+        }
+        new
+    }
+}
+
+fn main() {
+    let input = std::fs::read_to_string("../input.txt").unwrap();
+    let map: Vec<Vec<State>> = input
+        .split("\n")
+        .filter_map(|l| {
+            if l.is_empty() {
+                None
+            } else {
+                Some(l.chars().map(State::from).collect())
+            }
+        })
+        .collect();
+    let mut old_map = Map(map);
+
+    loop {
+        let map = old_map.tick();
+        if map == old_map {
+            println!(
+                "{}",
+                map.0
+                    .into_iter()
+                    .flatten()
+                    .filter(|&s| s == State::Occupied)
+                    .count()
+            );
+            return;
+        }
+        old_map = map;
+    }
+}
